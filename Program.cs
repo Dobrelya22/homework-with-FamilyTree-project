f2ee31883a35eaec5d1112b8978856235e937a01
Dobrelya22 <ostap.andrey21@gmail.com>
2024-05-05 15:08:23 +0300

    Implemented solution for the given task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] sourceArray = { "Hello", "2", "world", ":-)" };
+        string[] resultArray = new string[sourceArray.Length];
+        
+        int resultIndex = 0;
+        foreach (var item in sourceArray)
+        {
+            if (item.Length <= 3)
+            {
+                resultArray[resultIndex++] = item;
+            }
+        }
+
+        
+        Array.Resize(ref resultArray, resultIndex);
+
+        
+        Console.WriteLine("[" + string.Join(", ", sourceArray) + "] → [" + string.Join(", ", resultArray) + "]");
+    }
+}
